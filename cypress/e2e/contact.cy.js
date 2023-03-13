455bb24014b3438238939e59d9b398160c3ed317
Maksym Shokin <problaze7@gmail.com>
2023-03-13 22:42:43 +0200

    chaining

--- cypress/e2e/contact.cy.js
+++ cypress/e2e/contact.cy.js
@@ -6,13 +6,12 @@ describe('contact form', () => {
     cy.get('[data-cy="contact-input-message"]').type('Hello world');
     cy.get('[data-cy="contact-input-name"]').type('Big Boss');
     cy.get('[data-cy="contact-input-email"]').type('123@123.com');
-    cy.get('[data-cy="contact-btn-submit"]').contains('Send Message');
-    cy.get('[data-cy="contact-btn-submit"]').should(
-      'not.have.attr',
-      'disabled'
-    );
+    cy.get('[data-cy="contact-btn-submit"]')
+      .contains('Send Message')
+      .should('not.have.attr', 'disabled');
     cy.get('[data-cy="contact-btn-submit"]').click();
-    cy.get('[data-cy="contact-btn-submit"]').contains('Sending...');
-    cy.get('[data-cy="contact-btn-submit"]').should('have.attr', 'disabled');
+    cy.get('[data-cy="contact-btn-submit"]')
+      .contains('Sending...')
+      .should('have.attr', 'disabled');
   });
 });
