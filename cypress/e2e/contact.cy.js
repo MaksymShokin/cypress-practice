b804d9a907952a45a8959a8715c88e8d3141e309
Maksym Shokin <problaze7@gmail.com>
2023-03-14 09:47:49 +0200

    checking-for-focus

--- cypress/e2e/contact.cy.js
+++ cypress/e2e/contact.cy.js
@@ -30,4 +30,37 @@ describe('contact form', () => {
       .contains('Sending...')
       .should('have.attr', 'disabled');
   });
+
+  it('should validate the form input', () => {
+    cy.visit('http://localhost:5173/about');
+    cy.get('[data-cy="contact-btn-submit"]').click();
+    cy.get('[data-cy="contact-btn-submit"]').then(el => {
+      expect(el).to.not.have.attr('disabled');
+      expect(el.text()).to.not.equal('Sending...');
+    });
+
+    cy.get('[data-cy="contact-input-message"]').focus();
+    cy.get('[data-cy="contact-input-message"]').blur();
+    cy.get('[data-cy="contact-input-message"]')
+      .parent()
+      .then(el => {
+        expect(el.attr('class')).to.contain('invalid');
+      });
+
+    cy.get('[data-cy="contact-input-email"]').focus();
+    cy.get('[data-cy="contact-input-email"]').blur();
+    cy.get('[data-cy="contact-input-email"]')
+      .parent()
+      .then(el => {
+        expect(el.attr('class')).to.contain('invalid');
+      });
+
+    cy.get('[data-cy="contact-input-name"]').focus();
+    cy.get('[data-cy="contact-input-name"]').blur();
+    cy.get('[data-cy="contact-input-name"]')
+      .parent()
+      .then(el => {
+        expect(el.attr('class')).to.contain('invalid');
+      });
+  });
 });
